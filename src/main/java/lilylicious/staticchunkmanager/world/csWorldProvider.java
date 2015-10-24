b9a39ab3fcc2f91dd4843930948aa0c1c9d7f4b1
sinkillerj <sinkillerj@users.noreply.github.com>
2015-10-24 01:49:11 -0400

    Wops

--- src/main/java/lilylicious/staticchunkmanager/world/csWorldProvider.java
+++ src/main/java/lilylicious/staticchunkmanager/world/csWorldProvider.java
@@ -15,7 +15,7 @@ public class csWorldProvider extends WorldProviderSurface {
         }
         else
         {
-            return terrainType.getChunkGenerator(worldObj, field_82913_c);
+            return terrainType.getChunkGenerator(this.worldObj, this.field_82913_c);
         }
     }
 
