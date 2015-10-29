8f16f7c42d9424f1db5a7bbd2cc570404ea057bf
Dwargkiller <peter.hammerskog@gmail.com>
2015-10-29 17:34:23 +0100

    Fix inputstream reading/editing

--- src/main/java/lilylicious/staticchunkmanager/api/ExampleRequests.java
+++ src/main/java/lilylicious/staticchunkmanager/api/ExampleRequests.java
@@ -9,17 +9,20 @@ public class ExampleRequests {
 
     public static void requestChunks() {
         InputStream is;
-        
-        is = ExampleRequests.class.getResourceAsStream("/assets/staticchunkmanager/storedchunks/stairUpS.nbt");
-        csAPI.requestChunk(new ChunkRequest(is, 0, 0, true));
-        is = ExampleRequests.class.getResourceAsStream("/assets/staticchunkmanager/storedchunks/snMid.nbt");
-        csAPI.requestChunk(new ChunkRequest(is, 0, 1, true));
-        is = ExampleRequests.class.getResourceAsStream("/assets/staticchunkmanager/storedchunks/snMid.nbt");
-        csAPI.requestChunk(new ChunkRequest(is, 0, 2, true));
-        is = ExampleRequests.class.getResourceAsStream("/assets/staticchunkmanager/storedchunks/snMid.nbt");
-        csAPI.requestChunk(new ChunkRequest(is, 0, 3, true));
-        is = ExampleRequests.class.getResourceAsStream("/assets/staticchunkmanager/storedchunks/stairUpN.nbt");
-        csAPI.requestChunk(new ChunkRequest(is, 0, 4, true));
-        
+
+        try {
+            is = ExampleRequests.class.getResourceAsStream("/assets/staticchunkmanager/storedchunks/stairUpS.nbt");
+            csAPI.requestChunk(new ChunkRequest(is, 0, 0, true));
+            is = ExampleRequests.class.getResourceAsStream("/assets/staticchunkmanager/storedchunks/snMid.nbt");
+            csAPI.requestChunk(new ChunkRequest(is, 0, 1, true));
+            is = ExampleRequests.class.getResourceAsStream("/assets/staticchunkmanager/storedchunks/snMid.nbt");
+            csAPI.requestChunk(new ChunkRequest(is, 0, 2, true));
+            is = ExampleRequests.class.getResourceAsStream("/assets/staticchunkmanager/storedchunks/snMid.nbt");
+            csAPI.requestChunk(new ChunkRequest(is, 0, 3, true));
+            is = ExampleRequests.class.getResourceAsStream("/assets/staticchunkmanager/storedchunks/stairUpN.nbt");
+            csAPI.requestChunk(new ChunkRequest(is, 0, 4, true));
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
     }
 }
